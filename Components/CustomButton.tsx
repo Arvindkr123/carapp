98d40f1aa31ee4d06711c8f895925e56ddfd5f95
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-10-01 15:21:42 +0530

    added footer

--- Components/CustomButton.tsx
+++ Components/CustomButton.tsx
@@ -1,9 +1,9 @@
 "use client"
 import { CustomButtonProps } from "@/types"
 
-const CustomButton = ({ title, containerStyle, handleClick }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyle, handleClick, btnType }: CustomButtonProps) => {
     return (
-        <button disabled={false} type={"button"} className={`custom-btn ${containerStyle}`} onClick={handleClick}>
+        <button disabled={false} type={btnType} className={`custom-btn ${containerStyle}`} onClick={handleClick}>
             <span className="flex-1">{title}</span>
         </button>
     )
